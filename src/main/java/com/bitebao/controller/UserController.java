02263a90c7f2f5961073b66a4e6e7b0673a92436
jackson15988@gmail.com <536225Ab>
2020-09-08 15:15:25 +0800

    feat: 新增login html 登入頁面

--- src/main/java/com/bitebao/controller/UserController.java
+++ src/main/java/com/bitebao/controller/UserController.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 /*@Api(tags = "MvWatermarkController", description = "影片logo相關Api")*/
 @RestController
-@RequestMapping("/rest/mvWatermark")
+@RequestMapping("/admin/user")
 public class UserController {
     /*    @ApiOperation(value = "尋找預設浮水印", notes = "取得預設浮水印圖檔相關資料(位置、圖片位置、偏移量等)")*/
-    @GetMapping(path = "findDefaultMvWatermark")
-    public ResponseEntity<SuccessResponseDto> findDefaultMvWatermark() {
+    @GetMapping(path = "/dologin")
+    public ResponseEntity<SuccessResponseDto> dologin() {
+        System.out.println("進入login地址");
         return ResponseUtil.successOutputResult(null);
     }
 }
